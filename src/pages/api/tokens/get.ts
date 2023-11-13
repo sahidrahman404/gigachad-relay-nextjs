00ef62f679f7dd64b7ea7a4b115ff616f6382330
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-13 19:42:33 +0700

    fix api/tokens/get api

--- src/pages/api/tokens/get.ts
+++ src/pages/api/tokens/get.ts
@@ -1,11 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = string | null
+type Data = string | null;
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  res.status(200).json(req.headers.cookie ?? null)
+  // this will return `auth=${token}` || undefined
+  const cookie = req.headers.cookie;
+  res.status(200).json(cookie ? cookie.split("=")[1] : null);
 }
