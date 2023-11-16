bb570cab9668d629985642d65e59776d988698c2
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-16 12:29:36 +0700

    add AppPropsWithLayout type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,21 @@ import { useRelayNextjs } from 'relay-nextjs/app';
 import { getClientEnvironment } from '@/lib/relay_client_environment';
 import { ErrorBoundary } from 'react-error-boundary';
 import GlobalError from '@/components/Error/GlobalError';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+ 
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const { env, ...relayProps } = useRelayNextjs(pageProps, {
     createClientEnvironment: () => getClientEnvironment()!,
   });
-  // @ts-ignore
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(
     <ErrorBoundary fallback={<GlobalError />}>
