9f08520ca68c38bdb888be8102e510ebb4a8e546
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-22 22:41:32 +0700

    make sidebar sticky

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -44,7 +44,7 @@ export default function Sidebar({ className }: Sidebar) {
   const router = useRouter();
   return (
     <div className={cn("pb-12", className)}>
-      <div className="space-y-4 py-4">
+      <div className="space-y-4 py-4 sticky top-0">
         {sidebarConfig.map((sc) => {
           return (
             <div className="px-3 py-2" key={sc.section}>
