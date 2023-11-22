82d57e28a54c84031a941ba6618690dead48dc49
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-22 07:49:32 +0700

    add layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import Sidebar from "./Sidebar";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="h-full bg-background">
+      <div className="h-full grid lg:grid-cols-5">
+        <Sidebar className="hidden lg:block" />
+        <div className="h-full col-span-3 lg:col-span-4 lg:border-l">
+          <div className="h-full px-4 py-6 lg:px-8">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
