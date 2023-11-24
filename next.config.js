29e6f52f00e41b89aa9fec9cc70557613884970d
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-24 09:28:47 +0700

    turn off next's image optimization

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
       // Ensures no server modules are included on the client.
       config.plugins.push(
-        new webpack.IgnorePlugin({ resourceRegExp: /lib\/server/ })
+        new webpack.IgnorePlugin({ resourceRegExp: /lib\/server/ }),
       );
     }
 
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
