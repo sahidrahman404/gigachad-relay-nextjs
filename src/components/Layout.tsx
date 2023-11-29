1b8708941b4e828ed274e86ef1d24b09ade80198
Sahid Rahman <sahidrahman404@gmail.com>
2023-11-29 18:32:24 +0700

    add toaster to the layout page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Sidebar from "./Sidebar";
+import { Toaster } from "./ui/toaster";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -8,6 +9,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <Sidebar className="hidden lg:block" />
         <div className="h-full col-span-3 lg:col-span-4 lg:border-l">
           <div className="h-full px-4 py-6 lg:px-8">{children}</div>
+          <Toaster />
         </div>
       </div>
     </div>
