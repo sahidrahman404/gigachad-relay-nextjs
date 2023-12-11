3ed794d4ab11403d864543abc1956907eeb5fd86
Sahid Rahman <sahidrahman404@gmail.com>
2023-12-11 12:44:38 +0700

    update TimeField component styling

--- src/components/ReactAriaUI/TimeField.tsx
+++ src/components/ReactAriaUI/TimeField.tsx
@@ -24,7 +24,12 @@ function TimeField<T extends TimeValue>({
   return (
     <TimeFieldAria {...props}>
       <DateInput className="flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50">
-        {(segment) => <DateSegment segment={segment} />}
+        {(segment) => (
+          <DateSegment
+            segment={segment}
+            className="data-[focused]:bg-primary data-[focused]:text-primary-foreground data-[focused]:outline-none data-[focused]:rounded"
+          />
+        )}
       </DateInput>
     </TimeFieldAria>
   );
