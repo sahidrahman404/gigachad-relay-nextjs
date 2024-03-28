472408ad93ba3cbb31a26e8b9f8a5425d64da30e
Sahid Rahman <sahidrahman404@gmail.com>
2024-03-28 16:42:44 +0700

    fix editor height

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -7,10 +7,10 @@ function EditorField({ editor }: { editor: Editor | null }) {
       <div className="flex">
         <MenuBar editor={editor} className="mx-auto" />
       </div>
-      <div className="flex flex-col max-h-96 w-full rounded-md border border-input bg-transparent shadow-sm">
+      <div className="flex flex-col h-[50dvh] w-full rounded-md border border-input bg-transparent shadow-sm">
         <EditorContent
           editor={editor}
-          className="overflow-x-hidden overflow-y-auto py-5 px-4 hd-screen"
+          className="overflow-x-hidden overflow-y-auto py-5 px-4 min-h-full"
         />
       </div>
     </>
