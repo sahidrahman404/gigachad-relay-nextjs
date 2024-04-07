947cd46fc4a5a936f51bb3311902b192215da7a3
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-07 17:54:50 +0700

    hide stepper button on small devices

--- src/components/ReactAriaUI/NumberField.tsx
+++ src/components/ReactAriaUI/NumberField.tsx
@@ -29,11 +29,11 @@ function NumberField({
         {label}
       </Label>
       <Group className="flex w-full rounded data-[focus-within]:ring-2 data-[focus-within]:ring-ring">
-        <Button slot="decrement" variant="outline">
+        <Button slot="decrement" variant="outline" className="hidden md:block">
           -
         </Button>
         <Input className="flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50" />
-        <Button slot="increment" variant="outline">
+        <Button slot="increment" variant="outline" className="hidden md:block">
           +
         </Button>
       </Group>
