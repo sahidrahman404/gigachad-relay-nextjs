599b48f1f310704d7de1e604413c4254c50a50e6
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-08 20:38:29 +0700

    disable delete and edit button when isWorkingOut state true

--- src/components/Exercises/ExerciseAction.tsx
+++ src/components/Exercises/ExerciseAction.tsx
@@ -8,12 +8,17 @@ import {
 } from "@/components/ui/menubar";
 import Link from "next/link";
 import { DeleteExerciseDialog } from "./DeleteExerciseDialog";
+import { WorkoutMachineContext } from "../Layout";
 
 type ExerciseActionProps = {
   exerciseID: string;
 };
 
 function ExerciseAction({ exerciseID }: ExerciseActionProps) {
+  const isWorkingOut = WorkoutMachineContext.useSelector((state) =>
+    state.matches({ workingOut: {} }),
+  );
+
   return (
     <Menubar>
       <MenubarMenu>
@@ -23,7 +28,7 @@ function ExerciseAction({ exerciseID }: ExerciseActionProps) {
             <Link href={`/dashboard/exercises/${exerciseID}`}>View</Link>
           </MenubarItem>
           <MenubarSeparator />
-          <MenubarItem asChild>
+          <MenubarItem asChild disabled={isWorkingOut}>
             <Link href={`/dashboard/exercises/edit/${exerciseID}`}>Edit</Link>
           </MenubarItem>
           <MenubarSeparator />
@@ -32,6 +37,7 @@ function ExerciseAction({ exerciseID }: ExerciseActionProps) {
             Trigger={
               <MenubarItem
                 className="text-destructive focus:text-destructive"
+                disabled={isWorkingOut}
                 onSelect={(e) => {
                   e.preventDefault();
                 }}
