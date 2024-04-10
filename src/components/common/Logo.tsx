eda95e58d81d0f2fd10d187dfa10d901dcbfb44f
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-10 19:29:30 +0700

    fix: make logo component polymorphic

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
-export default function Logo({ href }: { href: string }) {
-    return (
-        <Link href={href}>
-            <img
-                className="h-10 w-auto"
-                src="/logo.svg"
-                alt="gigachad"
-            />
-        </Link>
-    )
+export default function Logo({
+  href,
+  className,
+  children,
+}: {
+  href: string;
+  className?: string;
+  children?: ReactNode;
+}) {
+  return (
+    <Link href={href} className={className}>
+      <img className="h-10 w-auto" src="/logo.svg" alt="gigachad" />
+      {children}
+    </Link>
+  );
 }
