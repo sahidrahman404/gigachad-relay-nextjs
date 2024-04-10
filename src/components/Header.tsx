422c9f4ead4601af9ada14b8b481ee52511516cf
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-10 19:30:37 +0700

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { MobileNav } from "./MobileNav";
+import Logo from "./common/Logo";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="ml-4 h-14 flex items-center">
+        <Logo
+          href="/"
+          className="hidden w-10 h-10 md:flex items-center space-x-2"
+        >
+          <span className="font-bold">Gigachad</span>
+        </Logo>
+        <MobileNav />
+      </div>
+    </header>
+  );
+}
+
+export { Header };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from "react";
-import Sidebar from "./Sidebar";
+import { Sidebar } from "./Sidebar";
 import { Toaster } from "./ui/sonner";
 import { createActorContext } from "@xstate/react";
 import { workoutMachine } from "@/lib/states/workoutMachine";
+import { Header } from "./Header";
 
 const WorkoutMachineContext = createActorContext(workoutMachine);
 
@@ -10,6 +11,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <WorkoutMachineContext.Provider>
       <div className="bg-background">
+        <Header />
         <div className="hd-screen grid lg:grid-cols-5">
           <Sidebar className="hidden lg:block" />
           <div className="col-span-3 lg:col-span-4 lg:border-l">
