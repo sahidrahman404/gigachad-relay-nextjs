6c9c81e1fb3c28e58592aae9e245676b466c12cf
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-14 19:22:46 +0700

    improve stepper button functionality

--- src/components/ReactAriaUI/NumberField.tsx
+++ src/components/ReactAriaUI/NumberField.tsx
@@ -31,27 +31,29 @@ function NumberField({
         {label}
       </Label>
       <Group className="flex w-full rounded data-[focus-within]:ring-2 data-[focus-within]:ring-ring">
-        {stepper && (
-          <Button
-            slot="decrement"
-            variant="outline"
-            className="hidden md:block"
-          >
-            -
-          </Button>
-        )}
         <Input className="flex items-center h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50" />
         {stepper && (
-          <Button
-            slot="increment"
-            variant="outline"
-            className="hidden md:block"
-          >
-            +
-          </Button>
+          <div className="flex flex-col">
+            <Button
+              slot="increment"
+              variant="outline"
+              className="h-4 rounded-none w-4 data-[pressed]:bg-green-800 opacity-50"
+            >
+              +
+            </Button>
+
+            <Button
+              slot="decrement"
+              variant="outline"
+              className="h-4 rounded-none w-4 data-[pressed]:bg-red-800 opacity-50"
+            >
+              -
+            </Button>
+          </div>
         )}
       </Group>
       {description && <Text slot="description">{description}</Text>}
+
       <FieldError>{errorMessage}</FieldError>
     </NumField>
   );
