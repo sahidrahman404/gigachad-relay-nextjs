a00a81eae7afbbe5bc4e7e12bcd95e9238142783
Sahid Rahman <sahidrahman404@gmail.com>
2024-04-16 14:47:42 +0700

    replace MenuMobile component with MenuResponsive

--- src/components/Exercises/ExerciseSort.tsx
+++ src/components/Exercises/ExerciseSort.tsx
@@ -1,9 +1,10 @@
 import { MenuTrigger } from "react-aria-components";
 import { Button } from "../ReactAriaUI/Button";
 import { ArrowUpDown } from "lucide-react";
-import { MenuMobile, MenuItem } from "../ReactAriaUI/Menu";
+import { MenuResponsive, MenuItem } from "../ReactAriaUI/Menu";
 import { ExercisesFilterSortProps } from "./ExercisesFilterSort";
 import { useState } from "react";
+import NonSSRWrapper from "../common/NonSSRWrapper";
 
 interface ExerciseSortProps
   extends Omit<
@@ -24,53 +25,55 @@ function ExerciseSort({
         <ArrowUpDown className="mr-2 w-4 h-4" />
         Sort
       </Button>
-      <MenuMobile
-        open={open}
-        setOpen={setOpen}
-        onAction={(key) => {
-          if (key === "Desc") {
-            startTransition(() => {
-              refetch({
-                orderby: "DESC",
-                exerciseTypeWhereInput:
-                  state.exerciseType.length !== 29
-                    ? []
-                    : { id: state.exerciseType },
-                musclesGroupWhereInput:
-                  state.musclesGroup.length !== 29
-                    ? []
-                    : { id: state.musclesGroup },
+      <NonSSRWrapper>
+        <MenuResponsive
+          open={open}
+          setOpen={setOpen}
+          onAction={(key) => {
+            if (key === "Desc") {
+              startTransition(() => {
+                refetch({
+                  orderby: "DESC",
+                  exerciseTypeWhereInput:
+                    state.exerciseType.length !== 29
+                      ? []
+                      : { id: state.exerciseType },
+                  musclesGroupWhereInput:
+                    state.musclesGroup.length !== 29
+                      ? []
+                      : { id: state.musclesGroup },
+                });
               });
-            });
-          }
-          if (key === "Asc") {
-            startTransition(() => {
-              refetch({
-                orderby: "ASC",
-                exerciseTypeWhereInput:
-                  state.exerciseType.length !== 29
-                    ? []
-                    : { id: state.exerciseType },
-                musclesGroupWhereInput:
-                  state.musclesGroup.length !== 29
-                    ? []
-                    : { id: state.musclesGroup },
+            }
+            if (key === "Asc") {
+              startTransition(() => {
+                refetch({
+                  orderby: "ASC",
+                  exerciseTypeWhereInput:
+                    state.exerciseType.length !== 29
+                      ? []
+                      : { id: state.exerciseType },
+                  musclesGroupWhereInput:
+                    state.musclesGroup.length !== 29
+                      ? []
+                      : { id: state.musclesGroup },
+                });
               });
+            }
+          }}
+          selectionMode="single"
+          selectedKeys={state.orderDirection}
+          onSelectionChange={(value) => {
+            dispatch({
+              type: "set_order_direction",
+              payload: value,
             });
-          }
-        }}
-        selectionMode="single"
-        selectedKeys={state.orderDirection}
-        onSelectionChange={(value) => {
-          dispatch({
-            type: "set_order_direction",
-            payload: value,
-          });
-        }}
-      >
-        <MenuItem id="Desc">Newest</MenuItem>
-        <MenuItem id="Asc">Oldest</MenuItem>
-      </MenuMobile>
+          }}
+        >
+          <MenuItem id="Desc">Newest</MenuItem>
+          <MenuItem id="Asc">Oldest</MenuItem>
+        </MenuResponsive>
+      </NonSSRWrapper>
     </MenuTrigger>
   );
 }
